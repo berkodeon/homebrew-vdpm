dd1786a3b173319597ae65c99f4fbfdc12ee8d48
Berk Odeon <berkodeon@gmail.com>
2026-08-30 19:30:57 +0200

    Add vdpm formula

---
+++ Formula/vdpm.rb
@@ -0,0 +1,31 @@
+class Vdpm < Formula
+  desc "CLI for managing VisiData plugins"
+  homepage "https://github.com/berkodeon/vdpm"
+  version "0.2.0"
+  license "Apache-2.0"
+
+  depends_on "visidata"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/berkodeon/vdpm/releases/download/v0.2.0/vdpm-v0.2.0-aarch64-apple-darwin.tar.gz"
+      sha256 "8844e5f40bdf31910b93afda19842118c3b711189a835baeaf29d86406eddb97"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/berkodeon/vdpm/releases/download/v0.2.0/vdpm-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "35030b263edf7bcd20e351822dbfca95dd9c5ba7edcddbe7c70e411e7fc83072"
+    end
+  end
+
+  def install
+    bin.install "vdpm"
+  end
+
+  test do
+    ENV["VDPM_HOME"] = testpath
+    assert_match "name", shell_output("#{bin}/vdpm list")
+  end
+end
